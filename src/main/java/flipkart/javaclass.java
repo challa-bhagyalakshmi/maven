0857f4de18856d57928f57cbfa5ccf5088b9156f
challa-bhagyalakkshmi <challabhagyalakshmi2298@gmail.com>
2022-07-16 17:04:09 +0530

    added files

---
+++ src/main/java/flipkart/javaclass.java
@@ -0,0 +1,11 @@
+package flipkart;
+
+public class javaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("my java project");
+	}
+
+}
